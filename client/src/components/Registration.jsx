d2e44504e88e3b8d3794c46ce82bd326d9e5d2c0
miku <svartkatt39@gmail.com>
2020-10-07 11:37:37 +0900

    make a rough scetch for registration form

--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
-export default function Registration() {
-    return (
-        <div>
-            
+function Resisration() {
+  return (
+    <div className="Resisration">
+      <div>
+        <div className="container">
+          <p>this is resisraiton</p>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Group controlId="form_fisrt_name">
+                  <Form.Label>first name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter first name" />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="form_last_name">
+                  <Form.Label>last name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter last name" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group controlId="form_tell">
+              <Form.Label>telephone number</Form.Label>
+              <Form.Control type="text" placeholder="Enter tell" />
+            </Form.Group>
+            <Form.Group controlId="form_email">
+              <Form.Label>email</Form.Label>
+              <Form.Control type="text" placeholder="Enter Place" />
+            </Form.Group>
+          </Form>
+          <Button variant="success" type="submit">
+            Click
+          </Button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
+
+export default Resisration;
