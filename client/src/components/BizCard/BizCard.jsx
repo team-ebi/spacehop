6f9bb08fe3a53490c7408a1d6e44aff111fbb608
Tanner Haberl <thaberl13@gmail.com>
2020-10-07 19:27:20 +0900

    updated css for bizcard component

--- client/src/components/BizCard/BizCard.jsx
+++ client/src/components/BizCard/BizCard.jsx
@@ -17,19 +17,19 @@ const [bizData, setBizData] = useState([{
 
     return (
         <div id="location-container">
-            {bizData.map((biz, index)=>( <ul id="location-cell">
+            {bizData.map((biz, index)=>( <div id="location-cell">
             <img id="business-image" src="https://www.japan-guide.com/g9/2005_01b.jpg"/> <br/>
              {biz.Name} <br/>
-            <img id="location-png" src="https://img.pngio.com/address-logo-png-105-images-in-collection-page-2-address-logo-png-730_980.png"/> {biz.Address}  <br/>
-            <img id="location-png" src="https://simpleicon.com/wp-content/uploads/building-3.png"/> {biz.Type} <br/>
+            <img id="location-png" /> {biz.Address}  <br/>
+            <img id="location-png" /> {biz.Type} <br/>
             Availability: 
             <select id="days">
                 {/* loop through fetched data */}
                 <option value="item">Monday</option>
             </select> <br/>
-            <img  id="location-png"src="https://www.pngrepo.com/download/100889/yen-currency-symbol.png"/> {biz.Price} <br/>
+            <img  id="location-png"/> {biz.Price} <br/>
             <input type="button" value="Book"></input>
-            </ul>))}
+            </div>))}
         </div>
     )
 }
