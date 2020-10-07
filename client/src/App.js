f774046a4bd9f6577c92bff5fa07bfe149144240
Tanner Haberl <thaberl13@gmail.com>
2020-10-07 17:00:19 +0900

    created display cards for business locations

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BizCard from './components/BizCard';
+import List from './components/List'
 
 
 function App() {
   return (
     <div className="App">
-        <BizCard/>
+        <List></List>
     </div>
   );
 }
--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import Data from '../data/businesses'
 export default function List() {
+const [data, setData] = useState(Data);
+
     return (
-        <div>
-            
+        <div >
+        <h1>List View</h1>
+            <div id="location-container">
+            {data.map((biz, index)=>( 
+            <div id="location-cell">
+            <img id="business-image" src="https://www.japan-guide.com/g9/2005_01b.jpg"/> <br/>
+             <span id="location-name">{biz.name}</span> <br/>
+             <img id="location-png" src="https://simpleicon.com/wp-content/uploads/building-3.png"/>
+            {biz.address_city} <br/>
+            <img id="location-png" src="https://img.pngio.com/address-logo-png-105-images-in-collection-page-2-address-logo-png-730_980.png"/>
+             {biz.address_street} <br/>
+             {biz.address_zip} <br/>
+             <img id="location-png" src="https://icons-for-free.com/iconfiles/png/512/furniture+office+seat+icon+icon-1320196644527887723.png"/> {biz.business_type} <br/>
+            <img id="location-png" src="https://static.thenounproject.com/png/780051-200.png"/>
+            {biz.phone} <br/>
+            Availability: 
+            <select id="days">
+                {biz.all_availabilities.map((avail, index)=>(
+                    <option>{avail.day}</option>
+                ))}
+            </select> <br/>
+            <img  id="location-png"src="https://www.pngrepo.com/download/100889/yen-currency-symbol.png"/> {biz.price} <br/>
+            <input type="button" value="Book" id="book-button" ></input>
+            </div>))}
+        </div>
         </div>
     )
 }
+
