67a1568d4bce0562d13453bc8929a882f8b370c1
Tanner Haberl <thaberl13@gmail.com>
2020-10-07 20:15:59 +0900

    pulling from master

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Search from "./components/Search";
 import List from "./components/List";
+import BizCard from "./components/BizCard/BizCard";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Nav />
         <Search />
       </div>
-      <List />
+      <BizCard/>
     </div>
   );
 }
