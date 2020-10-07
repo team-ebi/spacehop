1ec3a45bfe3a09c742775ad2a49292afbdc0ac92
Catherine Nuguid <catnuguid@gmail.com>
2020-10-08 02:01:28 +0900

    add autofill location search

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,20 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
 
 export default function Search() {
+  const [location, setLocation] = useState("");
+  const [coordinates, setCoordinates] = useState({lat: null, lng: null})
+
+  const handleSelect = async value => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setLocation(value);
+    setCoordinates(latLng);
+  };
+
   return (
     <div id="search-container">
       <div id="search-bar">
         <div className="input" id="location-input">
           <div className="input-header field">Location</div>
           <div>
-            <input
-              type="dropdown"
-              className="field"
-              placeholder="Where to?"
-            ></input>
+            <PlacesAutocomplete
+              value={location}
+              onChange={setLocation}
+              onSelect={handleSelect}
+            >
+              {({
+                getInputProps,
+                suggestions,
+                getSuggestionItemProps,
+                loading,
+              }) => (
+                <div>
+                  <input id="location-input" {...getInputProps({ placeholder: "Where to?" })} />
+                  <div id="autocomplete-selections">
+                    {loading ? <div>...loading</div> : null}
+                    {suggestions.map((suggestion) => {
+                      const style = {
+                        backgroundColor: suggestion.active ? "#80cc37" : "white",
+                        padding: "7px",
+                      }
+                      return <div {...getSuggestionItemProps(suggestion, {style})}>{suggestion.description}</div>
+                    })}
+                  </div>
+                </div>
+              )}
+            </PlacesAutocomplete>
           </div>
         </div>
         <div className="input">
@@ -30,12 +65,12 @@ export default function Search() {
 
         {/* when this button is clicked, list of available
         businesses will be displayed */}
-        <div id="search-button">
+        <button id="search-button">
           <div>
             <FontAwesomeIcon icon={faSearch} size="lg" />
           </div>
           <div id="search-text">Search</div>
-        </div>
+        </button>
       </div>
     </div>
   );
