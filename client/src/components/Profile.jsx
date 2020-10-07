632422a7808f62fabb3dd60596aa6981c47b3c15
miku <svartkatt39@gmail.com>
2020-10-07 11:30:13 +0900

    made a rough schetch for profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Button, Container, Row, Col, Image } from "react-bootstrap";
+import personImg from "../img/personal-2.jpg";
 
-export default function Profile() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Profile() {
+  return (
+    <div className="Profile">
+      <p>this is profile</p>
+      <div className="container">
+        <Container>
+          <Row>
+            <Image src={personImg} rounded />
+          </Row>
+          <Row>
+            <h4>hello I'm bra bra bra ....</h4>
+          </Row>
+          <Row>
+            <h2>name</h2>
+          </Row>
+          <Row>
+            <h2>telephone number</h2>
+          </Row>
+          <Row>
+            <h2>email</h2>
+          </Row>
+          <Row>
+            <h1>here is the feature</h1>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
+
+export default Profile;
