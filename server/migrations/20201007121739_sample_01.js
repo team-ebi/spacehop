c17ca1d7e9b6efc4fa55b6185e485e42d7aac1e4
yusuke99 <sendyusukeamail@gmail.com>
2020-10-07 12:26:10 +0900

    create: migration file

---
+++ server/migrations/20201007121739_sample_01.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("businesses", (table) => {
+    table.increments();
+    table.string("name");
+    table.string("address_street");
+    table.string("address_city");
+    table.string("address_zip");
+    table.string("phone");
+    table.string("business_type");
+    table.integer("capacity");
+    table.integer("price");
+  });
+};
+
+exports.down = function(knex) {
+  
+};
