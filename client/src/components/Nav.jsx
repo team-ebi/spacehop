7a29a0e294d742c494ee098298d2722a04e25b76
Catherine Nuguid <catnuguid@gmail.com>
2020-10-07 14:27:29 +0900

    build out navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../styles/Nav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import logo from "../images/logo.png"
 
 export default function Nav() {
-    return (
-        <div>
-            
+  const [displayMenu, setDisplayMenu] = useState(false);
+
+  return (
+    <nav>
+      <div id="navbar">
+        <div id="logo-container"><img id="logo"src={logo}/></div>
+        <div id="menu-container" onClick={() => setDisplayMenu(!displayMenu)}>
+          <div id="menu-button">
+            <FontAwesomeIcon icon={faBars} size="lg" color="#80CC37" />
+          </div>
+          <div id="profile-thumb">
+            <FontAwesomeIcon icon={faUserCircle} size="lg" color="#80CC37" />
+          </div>
         </div>
-    )
+      </div>
+      {displayMenu && 
+      <div id="menu">
+        <div className="menu-item" id="login">Log In</div>
+        <div className="menu-item">Sign Up</div>
+        <div className="menu-item">Profile</div>
+        <div className="menu-item">About</div>
+        <div className="menu-item">Team</div>
+      </div>}
+    </nav>
+  );
 }
