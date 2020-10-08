14d2ede7e318017769cd75c0418690b4f12dfd1c
yusuke99 <sendyusukeamail@gmail.com>
2020-10-08 12:00:10 +0900

    fix: var name

--- server/controllers/availability.js
+++ server/controllers/availability.js
@@ -7,21 +7,21 @@ router.get("/test", async(req, res) => {
 });
 
 router.get("/", async(req, res) => {
-  const req_day = req.body.day;
-  const req_city = req.body.address_city;
-  const req_start_hour = req.body.start_hour;
-  const req_end_hour = req.body.end_hour;
+  const day = req.body.day;
+  const address_city = req.body.address_city;
+  const start_hour = req.body.start_hour;
+  const end_hour = req.body.end_hour;
   
   const businesses = await db
   .select("*")
   .table("businesses")
   .join("availability", { "availability.business_id": "businesses.id" })
   .where({
-    day: req_day,
-    address_city: req_city,
+    day,
+    address_city
   })
-  .where("start_hour", "<=", req_start_hour)
-  .andWhere("end_hour", ">=", req_end_hour);
+  .where("start_hour", "<=", start_hour)
+  .andWhere("end_hour", ">=", end_hour);
   
   res.send(businesses);
 });
