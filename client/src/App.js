d2c28d7e2bd753d00465b67713d0c49fce8d573d
miku <svartkatt39@gmail.com>
2020-10-08 20:15:09 +0900

    fix code in src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
-import Search from "./components/Search";
+import Search from "./components/Search/Search";
 import List from "./components/List/List";
 import BizCard from "./components/BizCard/BizCard";
 
