a09d6c37136068fe49ca89a5c050a08ba9450372
yusuke99 <sendyusukeamail@gmail.com>
2020-10-08 11:50:14 +0900

    create: get availability by day, city, start hour and end hour

--- server/controllers/availability.js
+++ server/controllers/availability.js
@@ -8,17 +8,22 @@ router.get("/test", async(req, res) => {
 
 router.get("/", async(req, res) => {
   const req_day = req.body.day;
-
-  try {
-    const availability = await db
-    .select("*")
-    .table("availability")
-    .where({ day: req_day });
+  const req_city = req.body.address_city;
+  const req_start_hour = req.body.start_hour;
+  const req_end_hour = req.body.end_hour;
   
-    res.send(availability);
-  } catch(err) {
-    res.send("no match...");
-  }
+  const businesses = await db
+  .select("*")
+  .table("businesses")
+  .join("availability", { "availability.business_id": "businesses.id" })
+  .where({
+    day: req_day,
+    address_city: req_city,
+  })
+  .where("start_hour", "<=", req_start_hour)
+  .andWhere("end_hour", ">=", req_end_hour);
+  
+  res.send(businesses);
 });
 
 module.exports = router;
