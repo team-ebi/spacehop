4ceec1ffcf225768eb4db8ad60d4aeee4116100e
miku <svartkatt39@gmail.com>
2020-10-08 12:52:50 +0900

    add useContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Search from "./components/Search/Search";
 import List from "./components/List/List";
+import { AppContext } from "./AppContext";
 
 function App() {
+  // useContext
+  const [value, setValue] = useState("");
+
   return (
     <div className="App">
       <Nav />
       <Search />
       <List />
+      {/* useContext */}
+      <AppContext.Provider value={{ value, setValue }}>
+        useContext!
+      </AppContext.Provider>
     </div>
   );
 }
