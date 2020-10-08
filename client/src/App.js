0f976e1b70a57631075a2de85b6093c679c22fca
Tanner Haberl <thaberl13@gmail.com>
2020-10-08 11:51:13 +0900

    cleaned up bizcard component

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,17 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Search from "./components/Search";
 import List from "./components/List/List";
+import BizCard from "./components/BizCard/BizCard";
 
 function App() {
   return (
     <div className="App">
       <div id="overlay"></div>
       <div id="hero">
+        <BizCard/>
         <Nav />
         <Search />
       </div>
-      <List />
     </div>
   );
 }
