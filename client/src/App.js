602e7b8cddee2826634efb60d0515bf4b1a7f247
Tanner Haberl <thaberl13@gmail.com>
2020-10-08 13:45:26 +0900

    just used prettier to format

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
-<<<<<<< HEAD
-import Search from "./components/Search";
-=======
 import Search from "./components/Search/Search";
->>>>>>> 17479157f2d959fa67365ca71e0dbd6af56e7562
 import List from "./components/List/List";
 
 function App() {
