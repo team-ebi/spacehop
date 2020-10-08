f7e2c4e5464606d5ed90c1c5fc87cbc8e51d2071
yusuke99 <sendyusukeamail@gmail.com>
2020-10-08 20:42:36 +0900

    wip: aws connection

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
     client: 'postgresql',
     connection: `postgres://${process.env.USER}@127.0.0.1:5432/spacehop`
   },
   production: {
+    // TODO
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    connection: process.env.AWS_CONNECTION,
+    // host: process.env.AWS_HOST,
+    // port: process.env.AWS_PORT,
+    // user: process.env.AWS_USER,
+    // password: process.env.AWS_PASSWORD,
+    // database: process.env.AWS_DATABASE
   }
 };
