192d22dac4fc998ec727fbad9e77b02ad7578158
yusuke99 <sendyusukeamail@gmail.com>
2020-10-08 16:47:51 +0900

    fix: seed data

--- server/seeds/03_reservations.js
+++ server/seeds/03_reservations.js
@@ -23,6 +23,13 @@ exports.seed = function(knex) {
           business_id: 2,
           user_id: 2
         },
+        {
+          date: "2021-01-25",
+          price: 10000,
+          created_at: "2020-12-05",
+          business_id: 3,
+          user_id: 3
+        }
       ]);
     });
 };
