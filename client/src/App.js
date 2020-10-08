ba59f844beb25236f752077e5764e445d894b8ca
Catherine Nuguid <catnuguid@gmail.com>
2020-10-08 17:45:46 +0900

    login and signup auth pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,22 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Search from "./components/Search/Search";
 import List from "./components/List/List";
+import {
+  AmplifyAuthenticator,
+  AmplifySignIn,
+  withAuthenticator,
+  AmplifySignOut,
+} from "@aws-amplify/ui-react";
+import { I18n } from "aws-amplify";
 
-function App() {
+I18n.putVocabularies({
+  en: {
+    "Sign Out": "Log Out",
+    "Sign In": "Log In",
+  },
+});
+
+export default function App() {
   return (
     <div className="App">
       <Nav />
@@ -14,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+// export default withAuthenticator(App);
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,34 +2,69 @@ import React, { useState } from "react";
 import "./Nav.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { Auth } from "aws-amplify";
+import {
+  AmplifySignIn,
+  AmplifyAuthenticator,
+  AmplifySignOut,
+  AmplifySignUp,
+} from "@aws-amplify/ui-react";
+import logo from "../../images/logo.png";
 
 export default function Nav() {
   const [displayMenu, setDisplayMenu] = useState(false);
+  const [displayLogin, setDisplayLogin] = useState(false);
+
+  function handleLoginClick() {
+    setDisplayMenu(false);
+    setDisplayLogin(true);
+  }
+
+  function clearDisplay() {
+    setDisplayLogin(false);
+    setDisplayMenu(false);
+  }
+  
 
   return (
-    <nav>
-      <div id="navbar">
-        <div id="menu-container" onClick={() => setDisplayMenu(!displayMenu)}>
-          
-          {/* when button is clicked, small menu window will pop up */}
-          <div id="menu-button">
-            <FontAwesomeIcon icon={faBars} size="lg" color="#80CC37" />
-          </div>
-          <div id="profile-thumb">
-            <FontAwesomeIcon icon={faUserCircle} size="lg" color="#80CC37" />
+    <>
+      <div
+        className="overlay"
+        onClick={clearDisplay}
+      ></div>
+      <nav>
+        <div id="navbar">
+          <div id="menu-container" onClick={() => setDisplayMenu(true)}>
+            {/* when button is clicked, small menu window will pop up */}
+            <div id="menu-button">
+              <FontAwesomeIcon icon={faBars} size="lg" color="#80CC37" />
+            </div>
+            <div id="profile-thumb">
+              <FontAwesomeIcon icon={faUserCircle} size="lg" color="#80CC37" />
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* menu when user is not logged in yet */}
-      {displayMenu && 
-      <div id="menu">
-        <div className="menu-item" id="login">Log In</div>
-        <div className="menu-item">Sign Up</div>
-        <div className="menu-item">Profile</div>
-        <div className="menu-item">About</div>
-        <div className="menu-item">Team</div>
-      </div>}
-    </nav>
+        {/* menu when user is not logged in yet */}
+        {displayMenu && (
+          <div id="menu">
+            <button className="menu-item" id="login" onClick={handleLoginClick}>
+              Log in
+            </button>
+            <div className="menu-item">Sign Up</div>
+            <div className="menu-item">Profile</div>
+            <div className="menu-item">About</div>
+            <div className="menu-item">Team</div>
+            <AmplifySignOut />
+          </div>
+        )}
+
+        {displayLogin && (
+          <div id="login-window">
+            <AmplifySignIn usernameAlias="email" />
+          </div>
+        )}
+      </nav>
+    </>
   );
 }
