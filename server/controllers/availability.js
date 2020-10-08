ddaa3708ade573336a094a474b683905f6d024b6
yusuke99 <sendyusukeamail@gmail.com>
2020-10-08 10:57:28 +0900

    create: get availability by day

--- server/controllers/availability.js
+++ server/controllers/availability.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../src/knex.js");
 
 router.get("/test", async(req, res) => {
   res.send("working");
+});
+
+router.get("/", async(req, res) => {
+  const req_day = req.body.day;
+
+  try {
+    const availability = await db
+    .select("*")
+    .table("availability")
+    .where({ day: req_day });
+  
+    res.send(availability);
+  } catch(err) {
+    res.send("no match...");
+  }
 });
 
 module.exports = router;
