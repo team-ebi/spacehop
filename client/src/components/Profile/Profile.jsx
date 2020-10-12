aa13769060f77e0f3d7a92fd6f0c8efd9d43ce8f
Cat Nuguid <catnuguid@gmail.com>
2020-10-12 10:34:31 +0900

    format profile page and fix location input

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -38,113 +38,122 @@ function Profile() {
       >
         Profile
       </h2>
-      <div id="profile-info">
-        <div id="profile-img">
-          <FontAwesomeIcon
-            icon={faUserCircle}
-            size="10x"
-            color="darkslategrey"
-          />
-        </div>
-        <div id="profile-details">
-          <div className="detail">
-            <div id="profile-categories">First Name: </div>
-            {!displayInputs && (
-              <div className="attribute">
-                {user && user.attributes.given_name}
+      <div id="section-container">
+        <main>
+          <div id="profile-info">
+            <div id="profile-img">
+              <FontAwesomeIcon
+                icon={faUserCircle}
+                size="8x"
+                color="darkslategrey"
+              />
+            </div>
+            <div id="profile-details">
+              <div className="detail">
+                <div id="profile-categories">First Name: </div>
+                {!displayInputs && (
+                  <div className="attribute">
+                    {user && user.attributes.given_name}
+                  </div>
+                )}
+                {displayInputs && (
+                  <div className="attribute">
+                    <input
+                      className="profile-input"
+                      value={givenName}
+                      onChange={(e) => setGivenName(e.target.value)}
+                    />
+                  </div>
+                )}
               </div>
-            )}
-            {displayInputs && (
-              <div className="attribute">
-                <input
-                  className="profile-input"
-                  value={givenName}
-                  onChange={(e) => setGivenName(e.target.value)}
-                />
+              <div className="detail">
+                <div id="profile-categories">Last Name: </div>
+                {!displayInputs && (
+                  <div className="attribute">
+                    {user && user.attributes.family_name}
+                  </div>
+                )}
+                {displayInputs && (
+                  <div className="attribute">
+                    <input
+                      className="profile-input"
+                      value={familyName}
+                      onChange={(e) => setFamilyName(e.target.value)}
+                    />
+                  </div>
+                )}
               </div>
-            )}
+              <div className="detail">
+                <div id="profile-categories">Email: </div>
+                {!displayInputs && (
+                  <div className="attribute">
+                    {user && user.attributes.email}
+                  </div>
+                )}
+                {displayInputs && (
+                  <div className="attribute">
+                    <input
+                      className="profile-input"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="detail">
+                <div id="profile-categories">Phone Number: </div>
+                {!displayInputs && (
+                  <div className="attribute">
+                    {user && user.attributes.phone_number}
+                  </div>
+                )}
+                {displayInputs && (
+                  <div className="attribute">
+                    <input
+                      className="profile-input"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
-          <div className="detail">
-            <div id="profile-categories">Last Name: </div>
+          <div id="edit-button-container">
+            {/* this button will display only if user is not in editing mode */}
             {!displayInputs && (
-              <div className="attribute">
-                {user && user.attributes.family_name}
-              </div>
+              <button
+                id="edit-button"
+                onClick={() => setDisplayInputs(!displayInputs)}
+              >
+                Edit
+              </button>
             )}
-            {displayInputs && (
-              <div className="attribute">
-                <input
-                  className="profile-input"
-                  value={familyName}
-                  onChange={(e) => setFamilyName(e.target.value)}
-                />
-              </div>
-            )}
-          </div>
-          <div className="detail">
-            <div id="profile-categories">Email: </div>
-            {!displayInputs && (
-              <div className="attribute">{user && user.attributes.email}</div>
-            )}
-            {displayInputs && (
-              <div className="attribute">
-                <input
-                  className="profile-input"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-            )}
-          </div>
-          <div className="detail">
-            <div id="profile-categories">Phone Number: </div>
-            {!displayInputs && (
-              <div className="attribute">
-                {user && user.attributes.phone_number}
-              </div>
-            )}
-            {displayInputs && (
-              <div className="attribute">
-                <input
-                  className="profile-input"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                />
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-      <div id="edit-button-container">
-        {/* this button will display only if user is not in editing mode */}
-        {!displayInputs && (
-          <button
-            id="edit-button"
-            onClick={() => setDisplayInputs(!displayInputs)}
-          >
-            Edit
-          </button>
-        )}
 
-        {/* this button will display only if user is editing details
+            {/* this button will display only if user is editing details
         when clicked, it should post new details to user pool and db */}
-        {displayInputs && (
-          <button id="edit-button" onClick={updateProfile}>
-            Save
-          </button>
-        )}
+            {displayInputs && (
+              <button id="edit-button" onClick={updateProfile}>
+                Save
+              </button>
+            )}
+          </div>
+          {displayInputs && (
+            <div id="edit-button-container">
+              <button id="cancel" onClick={() => setDisplayInputs(false)}>
+                Cancel
+              </button>
+            </div>
+          )}
+        </main>
+        <hr id="profile-divider"></hr>
+        <section>
+          <FutureBookings />
+        </section>
       </div>
-      {displayInputs && (
-        <div id="edit-button-container">
-          <button id="cancel" onClick={() => setDisplayInputs(false)}>
-            Cancel
-          </button>
-        </div>
-      )}
 
       {/* future bookings is set in the middle of this code */}
       {/* future booking has its own branch : futureBookings */}
-      <FutureBookings />
     </div>
   );
 }
