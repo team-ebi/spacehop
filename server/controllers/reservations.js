a771c52d647801b8510471d85b62cbcc5d1c822f
Cat Nuguid <catnuguid@gmail.com>
2020-10-13 00:34:14 +0900

    fix endpoints for ratings and reservations

--- server/controllers/reservations.js
+++ server/controllers/reservations.js
@@ -12,6 +12,7 @@ router.post("/", async (req, res) => {
   //ex) req.body = { "email": "potato@dog.com", "date": "2020-12-30", "price": 1000, "business_id": 1 }
 
   // Get user id that matches with req.body.email
+ try {
   const email = req.body.email;
   const user = await db.select("*").table("users").where({
     email,
@@ -32,45 +33,46 @@ router.post("/", async (req, res) => {
   });
 
   res.send("New reservation created!");
+ } catch (err) {
+   console.log("ERROR posting reservation BE: ", err)
+ }
 });
 
 //Get selected user's reservations
-router.get("/reservations/:email", async (req, res) => {
-  const email = req.params.email;
-  const user = await db.select("*").table("users").where({
-    email,
-  });
+// router.get("/", async(req, res) => {
+//   const user_id = req.params.user_id;
 
-  const userId= user[0]["id"]
+//   const reservations = await db
+//   .select("*")
+//   .table("reservations")
+//   .where({
+//     user_id
+//   })
+//   .orderBy('date', 'asc');
 
-  const reservations = await db
-    .select("*")
-    .table("reservations")
-    .where({
-      userId
-    })
-    .orderBy("date", "asc");
+//   res.send(reservations);
+// });
 
-  res.send(reservations);
-});
+// Get user's upcoming reservations
+router.get("/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const user = await db.select("*").table("users").where("email", email);
 
-// Get all upcoming reservations
-router.get("/", async (req, res) => {
-  const email = req.body.email;
-  const user = await db.select("*").table("users").where({
-    email,
-  });
+    const user_id = user[0]["id"];
 
-  const user_id = user[0]["id"];
+    const upcomingReservations = await db
+      .select("*")
+      .table("businesses")
+      .join("reservations", { "businesses.id": "reservations.business_id" })
+      .join("availability", { "businesses.id": "availability.business_id" })
+      .join("users", { "reservations.user_id": "users.id" })
+      .where("reservations.user_id", user_id);
 
-  const upcomingReservations = await db
-    .select("*")
-    .table("businesses")
-    .join("reservations", { "businesses.id": "reservations.business_id" })
-    .join("users", { "reservations.user_id": "users.id" })
-    .where("reservations.user_id", user_id);
-
-  res.send(upcomingReservations);
+    res.send(upcomingReservations);
+  } catch (err) {
+    console.log("ERROR get reservation from BE: ", err);
+  }
 });
 
 //ISO 8601("2020-10-08T15:00:00.000Z") => YYYY-MM-DD
