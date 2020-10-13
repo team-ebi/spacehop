d7f738dc0e2cdd317b2609c2bdb51dce0b55c353
yusuke99 <sendyusukeamail@gmail.com>
2020-10-13 11:22:01 +0900

    fix: config

--- server/aws/migration.js
+++ server/aws/migration.js
@@ -1,14 +1,5 @@
 require("dotenv").config();
-
-const knex = require("knex")({
-  client: "postgresql",
-  connection: {
-    host: process.env.AWS_SPACEHOP_HOST,
-    user: process.env.AWS_SPACEHOP_USER,
-    password: process.env.AWS_SPACEHOP_PASSWORD,
-    database: process.env.AWS_SPACEHOP_DB
-  }
-});
+const knex = require("./config.js");
 
 knex.schema
 .dropTableIfExists("ratings")
