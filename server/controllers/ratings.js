13f631bbe874b74525356b219023e4138ed1dd7e
Cat Nuguid <catnuguid@gmail.com>
2020-10-13 11:32:31 +0900

    fix express endpoint to fetch rating by user email

--- server/controllers/ratings.js
+++ server/controllers/ratings.js
@@ -22,42 +22,46 @@ router.get("/:business_id", async (req, res) => {
 
 //Post selected business's rating
 router.post("/", async (req, res) => {
-  const email = req.body.email;
-  const user = await db.select("*").table("users").where({
-    email,
-  });
-
-  const user_id = user[0]["id"];
-  const business_id = req.body.business_id;
-  const point = req.body.point; //point is not null(1,2,3,4,5)
-  const comment = req.body.comment;
-
   try {
+    const email = req.body.email;
+    const user = await db.select("*").table("users").where({
+      email,
+    });
+
+    const user_id = user[0]["id"];
+    const business_id = req.body.business_id;
+    const point = req.body.point; //point is not null(1,2,3,4,5)
+    const comment = req.body.comment;
+
     const ratings = await db.select("*").table("ratings").insert({
       business_id,
       user_id,
       point,
       comment,
     });
-    res.send("Your rating posted!");
-  } catch {
-    res.sendStatus(500);
+    res.send(ratings);
+  } catch (err) {
+    console.log("ERROR posting rating BE: ", err);
   }
 });
 
 //Get selected business's rating by user
-router.get("/:business_id/:user_id", async (req, res) => {
-  const user_id = req.params.user_id;
-  const business_id = req.params.business_id;
-
+router.get("/:business_id/:email", async (req, res) => {
   try {
+    const email = req.params.email;
+
+    const user = await db.select("*").table("users").where("email", email);
+    
+    const user_id = user[0]["id"];
+    const business_id = req.params.business_id;
+
     const rating = await db.select("*").table("ratings").where({
       business_id,
       user_id,
     });
     res.send(rating);
-  } catch {
-    res.sendStatus(500);
+  } catch (err) {
+    console.log("ERROR fetching rating BE: ", err);
   }
 });
 
