a3298641554375993306a94a858669f0050a1a8e
yusuke99 <sendyusukeamail@gmail.com>
2020-10-13 14:28:11 +0900

    wip: fixing seed data

--- server/aws/03_users.js
+++ server/aws/03_users.js
@@ -3,22 +3,28 @@ const knex = require("./config.js");
 knex("users").del().then(() => {
   return knex("users").insert([
     {
+      first_name: "Masuo",
+      last_name: "Suzuki",
+      email: "m.suzuki@gmail.com",
+      phone: "080-7382-1819"
+    },
+    {
+      first_name: "Ryu",
+      last_name: "Tamura",
+      email: "r.tamura@gmail.com",
+      phone: "080-3332-1611"
+    },
+    {
+      first_name: "Akina",
+      last_name: "Ohira",
+      email: "a.ohira@gmail.com",
+      phone: "080-2202-0392"
+    },
+    {
       first_name: "Potato",
       last_name: "Fan",
       email: "potato@dog.com",
       phone: "090-7777-7777"
-    },
-    {
-      first_name: "Bobby",
-      last_name: "Boop",
-      email: "b.boop@gmail.com",
-      phone: "080-7777-7777"
-    },
-    {
-      first_name: "Frog",
-      last_name: "Green",
-      email: "frog@green.com",
-      phone: "070-7777-7777"
     }
   ]);
 })
