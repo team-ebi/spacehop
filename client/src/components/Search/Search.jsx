0ac905bcc1477c477df8d69a26aca09a2a7a4c02
noritaketakamichi <noritaket28@gmail.com>
2020-10-14 17:35:41 +0900

    add console.log

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -64,10 +64,11 @@ export default function Search() {
     // parse time from selected start time
     const endTime = new Date(selectedEndTime).getHours();
 
-    console.log("process.env.REACT_APP_BACKEND_URL:",process.env.REACT_APP_BACKEND_URL);
-    console.log("baseUrl:",baseUrl);
+    console.log("process.env.REACT_APP_BACKEND_URL: ",process.env.REACT_APP_BACKEND_URL);
+    console.log("baseUrl: ",baseUrl);
 
-    console.log("process.env.REACT_APP_REGION:",process.env.REACT_APP_REGION);
+    console.log("process.env.REACT_APP_REGION: ",process.env.REACT_APP_REGION);
+    console.log("process.env.NODE_ENV: ",process.env.NODE_ENV);
 
     // set data to axios.get(http://) then get filtered data
     const res = await axios.get(
