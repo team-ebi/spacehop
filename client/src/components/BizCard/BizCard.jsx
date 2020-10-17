850fc5974cc89c80ccd7bc5266fc3b769e8ac68b
miku <svartkatt39@gmail.com>
2020-10-17 16:50:48 +0900

    change props to biz

--- client/src/components/BizCard/BizCard.jsx
+++ client/src/components/BizCard/BizCard.jsx
@@ -124,7 +124,7 @@ export default function BizCard({ props }) {
               src="https://www.japan-guide.com/g9/2005_01b.jpg"
             />
           </div>
-          <BizCard_Map props={biz}/>
+          <BizCard_Map biZ={biz}/>
           <div>
             <div id="bizcard-name">
               <h2>{biz.name}</h2>
--- client/src/components/BizCard_Map/BizCard_Map.jsx
+++ client/src/components/BizCard_Map/BizCard_Map.jsx
@@ -3,19 +3,12 @@ import { GoogleMap, Marker, InfoWindow } from '@react-google-maps/api';
 import flag from '../../images/hopper_flag.png'; 
 
 
-function BizCard_Map({props}){
-    //[we need update here]
-    //shop data is in the props. We can plot the spot
-    //by accessing props.lat and props.lng when  backend modify data. 
-    //for now, only Ebi-chan has location property
-    console.log("props",props)
+function BizCard_Map({biz}){
 
-    //add lat, lng to props temporarily 
-    props.location = [{ lat:35.659871, lng:139.700662 }]
+    //add lat, lng to biz temporarily 
+    biz.location = [{ lat:35.659871, lng:139.700662 }]
     //add picture image 
-    props.img ="https://i.ibb.co/zhvKgwy/1280px-Bar-P1030319.jpg"
-    console.log("props added",props)
-    console.log("props lat",props.location[0].lat)
+    biz.img ="https://i.ibb.co/zhvKgwy/1280px-Bar-P1030319.jpg"
 
     const[selected, setSelected]=useState({});
 
@@ -30,7 +23,7 @@ function BizCard_Map({props}){
       
     //default center should be same as selected location 
       const defaultCenter = {
-        lat:props.location[0].lat, lng:props.location[0].lng
+        lat:biz.location[0].lat, lng:biz.location[0].lng
       }
 
       return (
@@ -40,11 +33,11 @@ function BizCard_Map({props}){
               zoom={16}
               center={defaultCenter}>
                 <Marker
-                key={props.name}
+                key={biz.name}
                 position={defaultCenter}
                 icon={flag}
                 onClick ={
-                    ()=>onSelect(props)
+                    ()=>onSelect(biz)
                 } />
 
 
