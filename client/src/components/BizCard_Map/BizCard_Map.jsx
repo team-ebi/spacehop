f19a35683fb32480e539646237ddc210ca26d0ad
miku <svartkatt39@gmail.com>
2020-10-17 12:21:00 +0900

    clickable flag is shown only in List component

--- client/src/components/BizCard_Map/BizCard_Map.jsx
+++ client/src/components/BizCard_Map/BizCard_Map.jsx
@@ -1,16 +1,19 @@
 import React, {useState } from 'react';
 import { GoogleMap, Marker, InfoWindow } from '@react-google-maps/api';
-import flag from '../../images/registration-mark.png'; 
+import flag from '../../images/hopper_flag.png'; 
 
 
 function BizCard_Map({props}){
-    //shop data is in props. We can plot the spot
-    //by accessing props.lat and props.lng 
+    //[we need update here]
+    //shop data is in the props. We can plot the spot
+    //by accessing props.lat and props.lng when  backend modify data. 
     //for now, only Ebi-chan has location property
     console.log("props",props)
 
-    //add lat, lng to props 
+    //add lat, lng to props temporarily 
     props.location = [{ lat:35.659871, lng:139.700662 }]
+    //add picture image 
+    props.img ="https://i.ibb.co/zhvKgwy/1280px-Bar-P1030319.jpg"
     console.log("props added",props)
     console.log("props lat",props.location[0].lat)
 
@@ -22,22 +25,20 @@ function BizCard_Map({props}){
 
     const mapStyles = {  
         margin:"0 auto",      
-        height: "30vh",
+        height: "35vh",
         width: "100%"};
       
-    //default center should be same as Biz card 
+    //default center should be same as selected location 
       const defaultCenter = {
         lat:props.location[0].lat, lng:props.location[0].lng
       }
 
-      console.log("selected location", selected.location)
       return (
           <div className="BizCard-Map-container" style={{width:"95%", marginTop:"20px"}}>
               <GoogleMap
               mapContainerStyle={mapStyles}
               zoom={16}
               center={defaultCenter}>
-                {/* tempolarly code ↓ */}
                 <Marker
                 key={props.name}
                 position={defaultCenter}
@@ -45,18 +46,9 @@ function BizCard_Map({props}){
                 onClick ={
                     ()=>onSelect(props)
                 } />
-                  {selected.location &&
-                  (
-                      <InfoWindow
-                    // if props has location like location:[{lat: ..., lng: ... )]
-                      position={selected.location[0]}
-                      clicable={true}
-                      onCloseClick={
-                          ()=>setSelected({})
-                      }>
-                      <p>Name: {props.name}</p>
-                      </InfoWindow>
-                  )}
+
+
+
               </GoogleMap>
           </div>
       )
