8731c1ad48bef74d1a633039daf098ef38042f30
noritaketakamichi <noritaket28@gmail.com>
2020-10-17 15:53:37 +0900

    drop and create database

--- server/test/_businesses.js
+++ server/test/_businesses.js
@@ -5,6 +5,22 @@ chai.use(chaiHttp);
 chai.should();
 const { setupServer } = require("../src/server");
 
+const db = require('knex');
+
+function getDbConnection() {
+  return db({
+    client: 'postgres',
+    // debug: true,
+    connection: {
+      host: "localhost",
+      database: "postgres",
+      port: "5432",
+      password: "",
+      user: "",
+    }
+  });
+}
+
 const config = {
   client: 'postgres',
   // debug: true,
@@ -21,8 +37,15 @@ const server = setupServer();
 
 describe("firstendpoint", () => {
   let request;
-
   const connection = require('knex')(config);
+
+  before(async () => {
+    const dbConnectionBefore = getDbConnection();
+    await dbConnectionBefore.raw('DROP DATABASE if exists spacehoptest');
+    await dbConnectionBefore.raw('CREATE DATABASE spacehoptest')
+    await dbConnectionBefore.destroy();
+  });
+
   beforeEach(async () => {
     request = chai.request(server);
 
@@ -34,15 +57,15 @@ describe("firstendpoint", () => {
     await connection.migrate.rollback()
   })
 
-it("just test", async () => {
-  const res = await request.get("/api/businesses/test")
-  const result = res.text;
-  expect(result).to.equal("working");
-});
+  it("just test", async () => {
+    const res = await request.get("/api/businesses/test")
+    const result = res.text;
+    expect(result).to.equal("working");
+  });
 
-it("count", async () => {
-  const res = await request.get("/api/businesses/data")
-  const result = res.body;
-  expect(result.length).to.equal(8);
-});
+  it("count", async () => {
+    const res = await request.get("/api/businesses/data")
+    const result = res.body;
+    expect(result.length).to.equal(8);
+  });
 });
