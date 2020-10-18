b570f110b26ec752581ab2dc94b8516e248ba357
Tanner Haberl <thaberl13@gmail.com>
2020-10-18 11:59:34 +0900

    changed css for message and inbox

--- client/src/components/Inbox/Inbox.jsx
+++ client/src/components/Inbox/Inbox.jsx
@@ -3,6 +3,7 @@ import "./Inbox.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { useHistory } from "react-router-dom";
+import Messages from "../Messages/Messages";
 
 export default function Inbox() {
   const [messages, setMessages] = useState([
@@ -14,40 +15,50 @@ export default function Inbox() {
     { sender: "Macchan", message: "Thanks." },
     { sender: "Ebi-chan", message: "Thank you sooooooo much!!!" },
     { sender: "Golden Ball", message: "You left a mess!!!!" },
-    { sender: "Macchan", message: "Thanks." }
+    { sender: "Macchan", message: "Thanks." },
   ]);
-  const [selectedMessage, setSelectedMessage] = useState();
+  const [selectedMessage, setSelectedMessage] = useState(messages[0])
   const history = useHistory();
 
   function messageHandler() {
-    console.log(selectedMessage)
-    return history.push("/inbox/message");
+    console.log(selectedMessage);
+    // return history.push("/inbox/message");
   }
 
   return (
-    <div id="inbox-container">
-      <h1 id="inbox-header">Messages</h1>
-      {messages.map((message, index) => {
-        return (
-          <ul id="message"
-            onClick={() => {
-              console.log(message[index]);
-              setSelectedMessage(message[index]);
-              messageHandler();
-            }}
-          >
-            <hr className="divider" />
-            <FontAwesomeIcon
-              className="icon"
-              icon={faUserCircle}
-              size="lg"
-              color="darkslategrey"
-            />
-            <span className="biz-name-message">{message.sender}</span> <br />
-            {message.message}
-          </ul>
-        );
-      })}
+    <div>
+      <div id="inbox-header">
+        <h1>Messages</h1>
+      </div>
+      <div id="message-container">
+        <div className="messages">
+          {messages.map((message, index) => {
+            return (
+              <ul
+                id="message"
+                onClick={() => {
+                  setSelectedMessage(messages[index]);
+                  messageHandler();
+                }}
+              >
+                <hr className="message-divider" />
+                <FontAwesomeIcon
+                  className="icon"
+                  icon={faUserCircle}
+                  size="lg"
+                  color="darkslategrey"
+                />
+                <span className="biz-name-message">{message.sender}</span>{" "}
+                <br />
+                {message.message}
+              </ul>
+            );
+          })}
+        </div>
+        <div className="selected-message-container">
+          <Messages selectedMessage={selectedMessage} />
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -1,34 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./Messages.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUserCircle
-} from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import moment from 'moment'
 
-export default function Messages({selectedMessage, setSelectedMessage}) {
-
-
-function messageHandler() {
-  console.log('hello')
-}
-
+export default function Messages({ selectedMessage, setSelectedMessage }) {
   return (
-    <div id="inbox-container">
-      <h1>Message</h1>
-      {/* <h1 id="inbox-header">Messages</h1>
-        {messages.map((message, index)=>{
-         return <ul onClick={messageHandler}>
-           <hr className="divider"/>
-           <FontAwesomeIcon
-                    className="icon"
-                    icon={faUserCircle}
-                    size="lg"
-                    color="darkslategrey"
-                  />
-           <span className="biz-name-message">{message.sender}</span>  <br/>
-            {message.message}
-          </ul>
-        })} */}
-    </div>
-  )
+    <>
+      {/* <h1 id="selected-message-header">Message</h1> */}
+      <div id="selected-message">
+        <div className="inbox-header">
+          <p className="biz-name-message">{selectedMessage.sender}</p>
+          <hr className="divider"/>
+        </div>
+        <div id="message-sender">{selectedMessage.sender}</div>
+        <div className="message-body-container">{selectedMessage.message}
+        <div id="message-time">  {moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>
+        </div>
+        <form onSubmit={(e)=>{
+          e.preventDefault()
+          console.log(e.target.message_input.value)
+        }} className="submit-container">
+          <input name="message_input" id="send-message-input" type="text" />
+          <button type="submit">
+          <FontAwesomeIcon
+            id="send-message-button"
+            icon={faPaperPlane}
+            size="lg"
+            color="darkslategrey"
+          />
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
