f84eda72c21c5e0ef93da157e033b6eb6f2f486c
Tanner Haberl <thaberl13@gmail.com>
2020-10-19 22:25:18 +0900

    created seed data and fetched data

--- client/src/components/Inbox/Inbox.jsx
+++ client/src/components/Inbox/Inbox.jsx
@@ -1,35 +1,39 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "./Inbox.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { useHistory } from "react-router-dom";
 import Messages from "../Messages/Messages";
 import { UserContext } from "../useContext/UserContext";
+import axios from "axios";
 
 export default function Inbox() {
-  const [messages, setMessages] = useState([{"user_messages":"Hello, do you have wi-fi at the izakaya?"}, {"business_messages": "Yes, we have unlimited wi-fi"}, {"user_messages" : "Sounds good! Thank you for responding."},  {"user_messages" : "I am sorry, I have one more question - Do you have many electrical outlets at the izakaya?"}, {"business_message" : "No problem! Yes we have many easily accessible outlets here and also have extension cords."}, {"user_messages" : "Oh, okay thank you. I will see you soon!"}, {"user_messages" : "Hello, I am sorry to message you but I think I forgot my laptop charger at your izakaya. Has anyone seen it?"}, {"business_messages": "Oh no, we are sorry to hear that! We will check if it is here now! We will get back to you soon."}, {"user_messages" : "Thank you so much."}, {"business_message" : "We have found your laptop charger! Would you like us to send it to you?"}, {"user_messages" : "Oh great! Thank you so much. I can come pick it up later today. Thank you again."}, {"business_message" : "Okay, sounds great. See you later."}]);
-
-  // [
-  //   { sender: "Ebi-chan", message: "Thank you!" },
-  //   { sender: "Golden Ball", message: "Please come again!" },
-  //   { sender: "Macchan", message: "Thanks." },
-  //   { sender: "Ebi-chan", message: "Thank you sooooooo much!!!" },
-  //   { sender: "Golden Ball", message: "You left a mess!!!!" },
-  //   { sender: "Macchan", message: "Thanks." },
-  //   { sender: "Ebi-chan", message: "Thank you sooooooo much!!!" },
-  //   { sender: "Golden Ball", message: "You left a mess!!!!" },
-  //   { sender: "Macchan", message: "Thanks." },
-  // ]
-
-  //user messages
-  const example = [{"user_messages":"Hello, do you have wi-fi at the izakaya?"}, {"business_messages": "Yes, we have unlimited wi-fi"}, {"user_messages" : "Sounds good! Thank you for responding."},  {"user_messages" : "I am sorry, I have one more question - Do you have many electrical outlets at the izakaya?"}, {"business_message" : "No problem! Yes we have many easily accessible outlets here and also have extension cords."}, {"user_messages" : "Oh, okay thank you. I will see you soon!"}, {"user_messages" : "Hello, I am sorry to message you but I think I forgot my laptop charger at your izakaya. Has anyone seen it?"}, {"business_messages": "Oh no, we are sorry to hear that! We will check if it is here now! We will get back to you soon."}, {"user_messages" : "Thank you so much."}, {"business_message" : "We have found your laptop charger! Would you like us to send it to you?"}, {"user_messages" : "Oh great! Thank you so much. I can come pick it up later today. Thank you again."}, {"business_message" : "Okay, sounds great. See you later."}]
-
-  // other random business inbox messages
-
-  const [selectedMessage, setSelectedMessage] = useState(messages[0])
+  const [selectedMessage, setSelectedMessage] = useState();
+  // const [selectedMessage, setSelectedMessage] = useState(messages[0]);
   const history = useHistory();
   const { user } = useContext(UserContext);
-  const [newMessage, setNewMessage] = useState()
+  const [newMessage, setNewMessage] = useState();
+  const [messages, setMessages] = useState([]);
+
+  // will connect to aws or default to loalhost
+  const baseUrl = "http://localhost:4000";
+  // const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:4000";
+
+
+  useEffect(() => {
+
+    async function fetchMessage() {
+      if (user) {
+        let req = axios.get(`${baseUrl}/api/messages/${user.attributes.email}`);
+        let res = await req;
+        let data = res.data;
+        console.log(data)
+        setMessages(data)
+      }
+    }
+    fetchMessage();
+  }, [user]);
+
 
   function messageHandler() {
     console.log(selectedMessage);
@@ -37,39 +41,6 @@ export default function Inbox() {
   }
 
   return (
-    <div>
-      <div id="inbox-header">
-        <h1>Messages</h1>
-      </div>
-      <div id="message-container">
-        <div className="messages">
-          {messages.map((message, index) => {
-            return (
-              <ul
-                id="message"
-                onClick={() => {
-                  setSelectedMessage(messages[index]);
-                  messageHandler();
-                }}
-              >
-                <hr className="message-divider" />
-                <FontAwesomeIcon
-                  className="icon"
-                  icon={faUserCircle}
-                  size="lg"
-                  color="darkslategrey"
-                />
-                <span className="biz-name-message">{message.sender}</span>{" "}
-                <br />
-                {message.message}
-              </ul>
-            );
-          })}
-        </div>
-        <div className="selected-message-container">
-          <Messages selectedMessage={selectedMessage} messages={messages} setMessages={setMessages} />
-        </div>
-      </div>
-    </div>
+  
   );
 }
--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -2,45 +2,54 @@ import React, { useState, useContext } from "react";
 import "./Messages.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-import moment from 'moment'
+import moment from "moment";
 import { UserContext } from "../useContext/UserContext";
+import axios from "axios";
 
-
-export default function Messages({ selectedMessage, setSelectedMessage, messages, setMessages }) {
- const { user } = useContext(UserContext);
+export default function Messages({
+  selectedMessage,
+  setSelectedMessage,
+  messages,
+  setMessages,
+}) {
+  const { user } = useContext(UserContext);
+  // const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:4000";
+  const baseUrl = "http://localhost:4000";
   return (
     <>
       {/* <h1 id="selected-message-header">Message</h1> */}
       <div id="selected-message">
         <div className="inbox-header">
-          <p className="biz-name-message">{selectedMessage.sender}</p>
-          <hr className="divider"/>
+          <p className="biz-name-message">"selectedMessage.sender"</p>
+          <hr className="divider" />
         </div>
-        <div id="message-sender">{selectedMessage.sender}</div>
-        <div className="message-body-container">{selectedMessage.message}
-        <div id="message-time">  {moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>
+        <div id="message-sender">"selectedMessage.sender"</div>
+        <div className="message-body-container">
+          "selectedMessage.message"
+          <div id="message-time">
+            {" "}
+            {moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}
+          </div>
         </div>
-        <form onSubmit={(e)=>{
-          e.preventDefault()
-          let newMsg = 
-            {
-              sender: user.attributes.email,
-              message: e.target.message_input.value
-            }
-          
-          console.log(messages)
-          console.log(newMsg)
-          setMessages([...messages, newMsg])
-          console.log("new messages, " + messages)
-        }} className="submit-container">
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            // await axios.patch(baseUrl + `/api/messages/${user.attributes.email}/:biz`, {
+            //   user_message: e.target.message_input.value
+            // } );
+            let newMsg = e.target.message_input.value
+            setMessages([...messages, newMsg]);
+          }}
+          className="submit-container"
+        >
           <input name="message_input" id="send-message-input" type="text" />
           <button type="submit">
-          <FontAwesomeIcon
-            id="send-message-button"
-            icon={faPaperPlane}
-            size="lg"
-            color="darkslategrey"
-          />
+            <FontAwesomeIcon
+              id="send-message-button"
+              icon={faPaperPlane}
+              size="lg"
+              color="darkslategrey"
+            />
           </button>
         </form>
       </div>
