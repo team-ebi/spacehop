f4789506b2433a05f435518f568ee42ac6ad5fe3
Tanner Haberl <thaberl13@gmail.com>
2020-10-19 22:26:39 +0900

    updated seed files and fetched data

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -10,6 +10,31 @@ router.get("/", async (req, res) => {
 });
 
 //get messages by user
+router.get("/:user", async (req, res) => {
+  const email = req.params.user
+  const user = await db
+  .select("*")
+  .table("users")
+  .where({email})
+  const user_id = user[0].id
+  const messages = await db
+    .select("*")
+    .table("messages")
+    .where({ user_id });
+  res.send(messages);
+});
+
+//get messages by biz
+router.get("/:user", async (req, res) => {
+  const business_id = req.params.biz;
+  const messages = await db
+    .select("*")
+    .table("messages")
+    .where({ biz_id });
+  res.send(messages);
+});
+
+//get messages by user and biz
 router.get("/:user/:biz", async (req, res) => {
   const business_id = req.params.biz;
   const user_id = req.params.user;
@@ -20,17 +45,55 @@ router.get("/:user/:biz", async (req, res) => {
   res.send(messages);
 });
 
-
 //update messages by user and biz
 router.patch("/:user/:biz", async (req, res) => {
-  const business_id = req.params.biz;
-  const user_id = req.params.user;
+  console.log(req.body)
+  const email = req.params.user
+  const user = await db
+  .select("*")
+  .table("users")
+  .where({email})
+  const user_id = user[0].id
+  const business_id = 1
   const updatedMessage = req.body;
+  console.log(updatedMessage)
   const updated = await db
     .select("*")
     .table("messages")
     .where({ business_id, user_id })
-    .update({ message: updatedMessage });
+    .update({ message: JSON.stringify(updatedMessage) });
+  
+  // const business_id = req.params.biz;
+  // const user_id = req.params.user;
+  // const updatedMessage = req.body;
+  // console.log(updatedMessage)
+  // const updated = await db
+  //   .select("*")
+  //   .table("messages")
+  //   .where({ business_id, user_id })
+  //   .update({ message: JSON.stringify(updatedMessage) });
+  res.status(200).end();
+});
+
+//post new message to db by user id and biz id
+router.post("/:user/:biz", async (req, res) => {
+  const business_id = req.params.biz;
+  const user_id = req.params.user;
+  const newMessage = req.body;
+  const check = await db
+    .select("*")
+    .table("messages")
+    .where({ business_id, user_id });
+  if (check.length === 0) {
+    const newConversation = await db
+      .select("*")
+      .table("messages")
+      .where({ business_id, user_id })
+      .insert({ 
+        business_id,
+         user_id,
+        message: newMessage })
+  }
   res.status(200).end();
 });
 
