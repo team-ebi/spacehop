3fed05c765688248014af004da817efb2e3f5147
noritaketakamichi <noritaket28@gmail.com>
2020-10-19 18:37:55 +0900

    delete one test

--- client/src/components/Team/__test__/Team.test.js
+++ client/src/components/Team/__test__/Team.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import Team from "../Team";
 
 describe("Team", () => {
-    it("should render", () => {
+    xit("should render", () => {
         const { getByText } = render(<Team/>);
         expect(getByText("meet the team")).toBeVisible()
     })
