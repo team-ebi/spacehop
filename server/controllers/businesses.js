d3d9aa461dd4503ac40300e9ce397091e64c9d82
yusuke99 <sendyusukeamail@gmail.com>
2020-10-26 15:53:00 +0900

    fix: lint

--- server/controllers/businesses.js
+++ server/controllers/businesses.js
@@ -40,27 +40,27 @@ router.post("/", async (req, res) => {
 
   // Get user id by email
   const user = await db
-    .select("*")
-    .table("users")
-    .where({ email });
+  .select("*")
+  .table("users")
+  .where({ email });
   const user_id = user[0]["id"];
 
   // Create business account, then create availability
   const business = await db
-    .select("*")
-    .table("businesses")
-    .returning("*")
-    .insert({
-      user_id,
-      name,
-      address_street,
-      address_city,
-      address_zip,
-      phone,
-      business_type,
-      capacity,
-      price,
-    });
+  .select("*")
+  .table("businesses")
+  .returning("*")
+  .insert({
+    user_id,
+    name,
+    address_street,
+    address_city,
+    address_zip,
+    phone,
+    business_type,
+    capacity,
+    price,
+  });
 
   // Get new business id 
   const business_id = business[0].id;
@@ -68,21 +68,21 @@ router.post("/", async (req, res) => {
   // Loop through availabilities and insert each one into availability table
   for (const availability of availabilities) {
     await db
-      .select("*")
-      .table("availability")
-      .insert({
-        business_id,
-        day: availability.day,
-        start_hour: availability.start_hour,
-        end_hour: availability.end_hour,
-      });
+    .select("*")
+    .table("availability")
+    .insert({
+      business_id,
+      day: availability.day,
+      start_hour: availability.start_hour,
+      end_hour: availability.end_hour,
+    });
   }
 
   // Fetch all availabilities
   const availability = await db
-    .select("*")
-    .table("availability")
-    .where({ business_id });
+  .select("*")
+  .table("availability")
+  .where({ business_id });
 
   // Add availability to business data that will be sent in response
   business[0]["availability"] = availability;
@@ -96,11 +96,10 @@ router.patch("/", async (req, res) => {
 
   // get user details from user table by email
   const user = await db
-    .select("*")
-    .returning("id")
-    .table("users")
-    .where({ email });
-
+  .select("*")
+  .returning("id")
+  .table("users")
+  .where({ email });
   const user_id = user[0]["id"];
 
   const updateInfo = req.body;
@@ -108,19 +107,19 @@ router.patch("/", async (req, res) => {
 
   // Update business info for user
   const businessInfo = await db
-    .select("*")
-    .returning("*")
-    .table("businesses")
-    .where({ user_id })
-    .update(updateInfo);
+  .select("*")
+  .returning("*")
+  .table("businesses")
+  .where({ user_id })
+  .update(updateInfo);
 
   // Get business id to fetch all reservations to be sent in response
   const business_id = businessInfo[0]["id"];
 
   const reservationInfo = await db
-    .select("*")
-    .table("reservations")
-    .where({ business_id });
+  .select("*")
+  .table("reservations")
+  .where({ business_id });
 
   // Combine business and reservation info
   businessInfo[0]["reservations"] = reservationInfo;
@@ -132,16 +131,16 @@ router.patch("/", async (req, res) => {
 router.delete("/", async (req, res) => {
   const email = req.body.email;
   const user = await db
-    .select("*")
-    .returning("id")
-    .table("users")
-    .where({ email });
+  .select("*")
+  .returning("id")
+  .table("users")
+  .where({ email });
   const user_id = user[0]["id"];
 
   await db
-    .table("businesses")
-    .where({ user_id })
-    .del();
+  .table("businesses")
+  .where({ user_id })
+  .del();
 
   res.send("Business information deleted");
 });
@@ -172,11 +171,11 @@ router.get("/:id/:date", async (req, res) => {
   const day = dayArray[dayOfNum];
 
   const BusinessesInfo = await db
-    .select("businesses.capacity", "availability.day", "availability.start_hour", "availability.end_hour")
-    .from("businesses")
-    .join("availability", { "availability.business_id": "businesses.id" })
-    .where("businesses.id", id)
-    .andWhere("availability.day", day);
+  .select("businesses.capacity", "availability.day", "availability.start_hour", "availability.end_hour")
+  .from("businesses")
+  .join("availability", { "availability.business_id": "businesses.id" })
+  .where("businesses.id", id)
+  .andWhere("availability.day", day);
 
   // If there is a data
   if (BusinessesInfo.length === 1) {
@@ -190,21 +189,20 @@ router.get("/:id/:date", async (req, res) => {
     //search each hour's left seats
     for (let i = start_hour; i < end_hour; i++) {
       reservationsAlready = await db
-        .count({ count: '*' })
-        .from("reservations")
-        .where({
-          "date": date,
-          "business_id": id,
-        })
-        .where("start_at", "<=", i)
-        .andWhere("end_at", ">=", i + 1);
+      .count({ count: '*' })
+      .from("reservations")
+      .where({
+        "date": date,
+        "business_id": id,
+      })
+      .where("start_at", "<=", i)
+      .andWhere("end_at", ">=", i + 1);
+
       const timeScale = String(i) + "-" + String(i + 1);
       timeObj[timeScale] = capacity - Number(reservationsAlready[0].count);
     }
 
-    //example
-    // {"12-13": 19,"13-14": 19,"14-15": 19,"15-16": 19,"16-17": 18}
-
+    // ex) { "12-13": 19,"13-14": 19,"14-15": 19,"15-16": 19,"16-17": 18 }
     res.send(timeObj);
   }else{
     // If not open that day, return empty object
