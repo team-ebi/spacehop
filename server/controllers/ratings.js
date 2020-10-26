11ccd80a94abad094202e08d01e9611e35c87dd1
yusuke99 <sendyusukeamail@gmail.com>
2020-10-26 15:57:23 +0900

    fix: lint

--- server/controllers/ratings.js
+++ server/controllers/ratings.js
@@ -5,14 +5,13 @@ const db = require("../src/knex.js");
 // Get selected business's ratings
 router.get("/:business_id", async (req, res) => {
   const business_id = req.params.business_id;
-
   try {
     const ratings = await db
-      .select("*")
-      .table("ratings")
-      .where({
-        business_id,
-      });
+    .select("*")
+    .table("ratings")
+    .where({
+      business_id
+    });
     res.send(ratings);
   } catch {
     res.sendStatus(500);
@@ -23,25 +22,29 @@ router.get("/:business_id", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const email = req.body.email;
+
     const user = await db
-      .select("*")
-      .table("users")
-      .where({
-        email,
-      });
+    .select("*")
+    .table("users")
+    .where({
+      email
+    });
     const user_id = user[0]["id"];
+
     const business_id = req.body.business_id;
     const point = req.body.point; // Point is not null (1,2,3,4,5)
     const comment = req.body.comment;
+
     const ratings = await db
-      .select("*")
-      .table("ratings")
-      .insert({
-        business_id,
-        user_id,
-        point,
-        comment,
-      });
+    .select("*")
+    .table("ratings")
+    .insert({
+      business_id,
+      user_id,
+      point,
+      comment
+    });
+    
     res.send(ratings);
   } catch {
     res.sendStatus(500);
@@ -52,19 +55,23 @@ router.post("/", async (req, res) => {
 router.get("/:business_id/:email", async (req, res) => {
   try {
     const email = req.params.email;
+
     const user = await db
-      .select("*")
-      .table("users")
-      .where("email", email);
+    .select("*")
+    .table("users")
+    .where("email", email);
     const user_id = user[0]["id"];
+
     const business_id = req.params.business_id;
+
     const rating = await db
-      .select("*")
-      .table("ratings")
-      .where({
-        business_id,
-        user_id,
-      });
+    .select("*")
+    .table("ratings")
+    .where({
+      business_id,
+      user_id,
+    });
+
     res.send(rating);
   } catch {
     res.sendStatus(500);
