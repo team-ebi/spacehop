2858f5018c8b8a6b6d2af715c5a838ff7ad916a3
yusuke99 <sendyusukeamail@gmail.com>
2020-10-26 15:37:39 +0900

    delete: unused modules, comments

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../src/knex.js");
-const moment = require("moment");
-const { returning } = require("../src/knex.js");
 
 router.get("/:user", async (req, res) => {
   let objToSend = {};
@@ -69,31 +67,6 @@ router.get("/", async (req, res) => {
   res.send(messages);
 });
 
-//get messages by user
-// router.get("/:user", async (req, res) => {
-//   const email = req.params.user
-//   const user = await db
-//   .select("*")
-//   .table("users")
-//   .where({email})
-//   const user_id = user[0].id
-//   const messages = await db
-//     .select("*")
-//     .table("messages")
-//     .where({ user_id });
-//   res.send(messages);
-// });
-
-//get messages by biz
-// router.get("/:user", async (req, res) => {
-//   const business_id = req.params.biz;
-//   const messages = await db
-//     .select("*")
-//     .table("messages")
-//     .where({ biz_id });
-//   res.send(messages);
-// });
-
 // Get messages by user and biz
 router.get("/:user/:biz", async (req, res) => {
   const business_id = req.params.biz;
@@ -121,16 +94,6 @@ router.patch("/:user/:biz", async (req, res) => {
     .table("messages")
     .where({ business_id, user_id })
     .update({ message: JSON.stringify(updatedMessage.message) });
-
-  // const business_id = req.params.biz;
-  // const user_id = req.params.user;
-  // const updatedMessage = req.body;
-  // console.log(updatedMessage)
-  // const updated = await db
-  //   .select("*")
-  //   .table("messages")
-  //   .where({ business_id, user_id })
-  //   .update({ message: JSON.stringify(updatedMessage) });
   res.status(200).end();
 });
 
@@ -143,6 +106,7 @@ router.post("/:user/:biz", async (req, res) => {
     .select("*")
     .table("messages")
     .where({ business_id, user_id });
+    
   if (check.length === 0) {
     const newConversation = await db
       .select("*")
