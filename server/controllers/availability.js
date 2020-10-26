2bc935883b55a4d8ecad1df06dcc14a00b41ea07
yusuke99 <sendyusukeamail@gmail.com>
2020-10-26 15:15:33 +0900

    delete: unused endpoints

--- server/controllers/availability.js
+++ server/controllers/availability.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("../src/knex.js");
 
-router.get("/test", async (req, res) => {
-  res.send("working");
-});
-
 router.get("/", async (req, res) => {
   const address_city = req.query.address_city;
   const start_hour = req.query.start_hour;
